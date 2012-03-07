fe92928beb6d5895da284fac9a88d25e71daa539
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-07 20:08:45 +0100

    add another text :)

--- ws/api.php
+++ ws/api.php
@@ -3,10 +3,13 @@ header('Content-Type:application/json');
 
 $data = Array(
     Array(
-        'text' => 'Wir melden uns heute live von der CeBIT'
+        'text' => 'Wir melden uns heute live von der CeBIT.'
     ),
     Array(
-        'text' => 'Die Teilnehmer der Veranstaltung betreten erstmal den roten Teppich'
+        'text' => 'Die Teilnehmer der Veranstaltung betreten erstmal den roten Teppich.'
+    ),
+    Array(
+        'text' => 'Auch bekannte Gesichter wie Mathias Plica konnten gesichtet werden!'
     )
 );
 
