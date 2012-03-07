d748c1c0357ee57328cb9d9c2ddf57de7e58ec94
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-07 21:46:56 +0100

    read posts from csv (to be abstracted)

--- ws/api.php
+++ ws/api.php
@@ -4,17 +4,13 @@ require_once(__DIR__ . '/../bootstrap.php');
 use Json\Codec;
 header('Content-Type:application/json');
 
-$data = Array(
-    Array(
-        'text' => 'Wir melden uns heute live von der CeBIT.'
-    ),
-    Array(
-        'text' => 'Die Teilnehmer der Veranstaltung betreten erstmal den roten Teppich.'
-    ),
-    Array(
-        'text' => 'Auch bekannte Gesichter wie Mathias Plica konnten gesichtet werden!'
-    )
-);
+$posts = Array();
+$file = fopen(__DIR__ . '/../posts.csv', 'r');
+if ($file) while (($line = fgetcsv($file, 0, ';', '"')) !== FALSE) {
+    $posts[] = Array(
+        'text' => $line[0]
+    );
+}
 
 $json = new Codec();
-echo $json->encode($data);
+echo $json->encode($posts);
