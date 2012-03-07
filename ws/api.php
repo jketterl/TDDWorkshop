ec41165f6b16dd33c0d8170de7c0db09fd42fc86
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-07 22:15:01 +0100

    remove unused code

--- ws/api.php
+++ ws/api.php
@@ -6,15 +6,6 @@ use Posts\PostList;
 header('Content-Type:application/json');
 
 $posts = Array();
-/*
-$file = fopen(__DIR__ . '/../posts.csv', 'r');
-if ($file) while (($line = fgetcsv($file, 0, ';', '"')) !== FALSE) {
-    $posts[] = Array(
-        'text' => $line[0]
-    );
-}
-*/
-
 $list = new PostList();
 foreach ($list as $post) $posts[] = $post;
 
