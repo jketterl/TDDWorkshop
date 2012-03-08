d7871bd87a6704c7ee9b6302648a22f732afb83f
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-08 21:57:57 +0100

    sample validator integration

--- ws/api.php
+++ ws/api.php
@@ -3,11 +3,17 @@ require_once(__DIR__ . '/../bootstrap.php');
 
 use Json\Codec;
 use Posts\PostList;
+use Validator\Profanity;
+
 header('Content-Type:application/json');
 
 $posts = Array();
 $list = new PostList();
-foreach ($list as $post) $posts[] = $post;
+$validator = new Profanity();
+
+foreach ($list as $post) {
+    if ($validator->isValid($post->getText())) $posts[] = $post;
+}
 
 $json = new Codec();
 echo $json->encode($posts);
