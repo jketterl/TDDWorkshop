fc86e5d50e21eb263723f2c9b5a3dac3110f41a9
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-10 23:00:59 +0100

    add rest client storage implementation (abstracted without client implemenation)

---
+++ library/Storage/RestStorage.php
@@ -0,0 +1,79 @@
+<?php
+namespace Storage;
+
+use Http\HttpClientInterface;
+
+class RestStorage implements StorageInterface
+{
+    protected $_client;
+    protected $_baseUrl;
+    protected $_className;
+    protected $_format;
+    
+    public function __construct($baseUrl, $className, $format = 'json')
+    {
+        $this->_baseUrl = $baseUrl;
+        $this->_className = $className;
+        $this->_format = $format;
+    }
+    
+    public function setClient(HttpClientInterface $client)
+    {
+        $this->_client = $client;
+        return $this;
+    }
+    
+    public function getClient()
+    {
+        return $this->_client;
+    }
+
+    public function load($id)
+    {
+        $data = $this->decode($this->getClient()->get($this->_baseUrl . '/' . $id . '.' . $this->_format));
+        return $this->toObject($data);
+    }
+    
+    public function store($object)
+    {
+        $data = Array();
+        foreach ($object as $key => $value) {
+            if ($key == 'id') continue;
+            $data[$key] = $value;
+        }
+        
+        if (isset($object->id)) {
+            $this->getClient()->post($this->_baseUrl . '/' . $object->id . '.' . $this->_format, $data);
+        } else {
+            $this->getClient()->put($this->_baseUrl . '.' . $this->_format, $data);
+        }
+    }
+    
+    public function getAll()
+    {
+        $response = $this->decode($this->getClient()->get($this->_baseUrl . '.' . $this->_format));
+        $result = Array();
+        foreach ($response as $data) {
+            $result[] = $this->toObject($data);
+        }
+        return $result;
+    }
+    
+    protected function decode($in)
+    {
+        // json_decode should not be hardcoded here, it should somehow be exchangeble (see the $format argument in the
+        // constructor) but it will work fine for demonstration purposes
+        return json_decode($in);
+    }
+    
+    protected function toObject($data)
+    {
+        $object = new $this->_className;
+        
+        foreach ($data as $key => $value) {
+            $object->$key = $value;
+        }
+        
+        return $object;
+    }
+}
---
+++ tests/Storage/RestStorageTest.php
@@ -0,0 +1,99 @@
+<?php
+namespace Storage;
+
+/**
+ * @author jketterl
+ * @group rest
+ */
+use Posts\User;
+
+class RestStorageTest extends \PHPUnit_Framework_TestCase
+{
+    protected $_client;
+    protected $_storage;
+    
+    protected function setUp()
+    {
+        $this->_client = $this->getMock('Http\HttpClientInterface');
+        $this->_storage = new RestStorage('http://localhost/rest/user', 'Posts\User');
+        $this->_storage->setClient($this->_client);
+    }
+    
+    public function testRetrievesList()
+    {
+        $json = json_encode(
+            Array(
+                Array(
+                    'id' => 1,
+                    'login' => 'atest',
+                    'name' => 'Alfred Testbenutzer'
+                ),
+                Array(
+                    'id' => 2,
+                    'login' => 'bbeispiel',
+                    'name' => 'Bernadette Beispiel'
+                )
+            )
+        );
+        $this->_client->expects($this->at(0))
+                      ->method('get')
+                      ->with('http://localhost/rest/user.json')
+                      ->will($this->returnValue($json));
+        
+        $users = $this->_storage->getAll();
+        self::assertEquals(2, count($users));
+        foreach ($users as $user) self::assertInstanceOf('Posts\User', $user);
+    }
+    
+    public function testRetrievesUser()
+    {
+        $json = json_encode(
+            Array(
+                'id' => 1,
+                'login' => 'atest',
+                'name' => 'Alfred Testbenutzer'
+            )
+        );
+        
+        $this->_client->expects($this->at(0))
+                      ->method('get')
+                      ->with('http://localhost/rest/user/42.json')
+                      ->will($this->returnValue($json));
+        
+        $user = $this->_storage->load(42);
+        self::assertInstanceOf('Posts\User', $user);
+        self::assertEquals('atest', $user->getLogin());
+        self::assertEquals('Alfred Testbenutzer', $user->getName());
+    }
+    
+    public function testPutsNewUser()
+    {
+        $user = new User();
+        $user->setLogin('atest')->setName('Alfons Testhuber');
+        
+        $this->_client->expects($this->at(0))
+                      ->method('put')
+                      ->with(
+                          'http://localhost/rest/user.json',
+                          Array('login' => 'atest', 'name' => 'Alfons Testhuber')
+                      );
+        
+        $this->_storage->store($user);
+    }
+    
+    public function testPostsExistingUser()
+    {
+        $user = new User();
+        $user->setLogin('atest')->setName('Alfons Testhuber');
+        $user->id = 42;
+        
+        $this->_client->expects($this->at(0))
+                      ->method('post')
+                      ->with(
+                          'http://localhost/rest/user/42.json',
+                          Array('login' => 'atest', 'name' => 'Alfons Testhuber')
+                      );
+        
+        $this->_storage->store($user);
+    }
+}
