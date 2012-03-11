a682318d5a66a1b0f97be59511f516103ca00edb
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-11 14:20:22 +0100

    lesson 1 code (again)

--- library/Json/Codec.php
+++ library/Json/Codec.php
@@ -3,13 +3,5 @@ namespace Json;
 
 class Codec implements CodecInterface
 {
-    public function encode($data)
-    {
-        return json_encode($data);
-    }
-    
-    public function decode($data)
-    {
-        return json_decode($data);
-    }
+    // IMPLEMENT LESSON 1 HERE
 }
