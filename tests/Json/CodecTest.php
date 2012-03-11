61e65137ba19aaeacb20db3ff8902a0d3ea792f5
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-11 14:31:34 +0100

    lesson 2 solution

--- tests/Json/CodecTest.php
+++ tests/Json/CodecTest.php
@@ -11,5 +11,27 @@ namespace Json;
  */
 class CodecTest extends \PHPUnit_Framework_TestCase
 {
-    // IMPLEMENT LESSON 2 HERE
+    public function testEncodesArray()
+    {
+        $encoder = new Codec();
+        $array = Array(
+            'text1',
+            'text2',
+            'text3'
+        );
+        
+        $output = $encoder->encode($array);
+        self::assertEquals('["text1","text2","text3"]', $output);
+    }
+    
+    public function testEncodesObject()
+    {
+        $encoder = new Codec();
+        $object = new \stdClass();
+        $object->foo = 'bar';
+        $object->bla = 'blubb';
+        
+        $output = $encoder->encode($object);
+        self::assertEquals('{"foo":"bar","bla":"blubb"}', $output);
+    }
 }
