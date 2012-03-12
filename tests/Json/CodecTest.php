8a44bab498fa09db6fbc5263f17d2c96643ee907
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-12 21:20:50 +0100

    remove annotations

--- tests/Json/CodecTest.php
+++ tests/Json/CodecTest.php
@@ -1,14 +1,6 @@
 <?php
 namespace Json;
 
-/**
- * @author jketterl
- * 
- * @cgroup encoders
- * @group json
- * 
- * @covers Json\Codec
- */
 class CodecTest extends \PHPUnit_Framework_TestCase
 {
     // IMPLEMENT LESSON 2 HERE
