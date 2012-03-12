0ecfeb93f32c18f904ccb231c6daa79cbfb8b6da
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-12 21:28:16 +0100

    add sample code as comment

--- tests/Json/CodecTest.php
+++ tests/Json/CodecTest.php
@@ -6,3 +6,20 @@ class CodecTest extends \PHPUnit_Framework_TestCase
 {
     // IMPLEMENT LESSON 2 HERE
 }
+
+/*
+
+// Das ist die Musterlösung aus Lektion 1. kann nach dem Portieren entfernt werden!
+
+$json = new Codec();
+
+var_dump(
+    $json->encode(Array('das ist ein Test')) == '["das ist ein Test"]'
+);
+
+$obj = new stdClass();
+$obj->name = 'Hans Wurst';
+var_dump(
+    $json->encode($obj) == '{"name":"Hans Wurst"}'
+);
+ */
