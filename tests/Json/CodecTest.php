afba25fb0b3ea61a54f8bf34642d02f516effc21
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-12 21:34:08 +0100

    add new ideal solution

--- tests/Json/CodecTest.php
+++ tests/Json/CodecTest.php
@@ -6,22 +6,26 @@ class CodecTest extends \PHPUnit_Framework_TestCase
 {
     public function testEncodesArray()
     {
-        $encoder = new Codec();
+        $json = new Codec();
         $array = Array(
             'das ist ein Test'
         );
         
-        $output = $encoder->encode($array);
-        self::assertEquals('["das ist ein Test"]', $output);
+        self::assertEquals(
+            '["das ist ein Test"]',
+            $json->encode($array)
+        );
     }
     
     public function testEncodesObject()
     {
-        $encoder = new Codec();
+        $json = new Codec();
         $object = new \stdClass();
         $object->name = 'Hans Wurst';
         
-        $output = $encoder->encode($object);
-        self::assertEquals('{"name":"Hans Wurst"}', $output);
+        self::assertEquals(
+            '{"name":"Hans Wurst"}',
+            $json->encode($object)
+        );
     }
 }
