73e77af60efc2cbd1a44dec974b0804346546531
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-12 22:34:28 +0100

    add phpunit.xml remove require_once() calls

--- tests/Json/CodecTest.php
+++ tests/Json/CodecTest.php
@@ -1,6 +1,5 @@
 <?php
 namespace Json;
-require_once(__DIR__ . '/../../bootstrap.php');
 
 class CodecTest extends \PHPUnit_Framework_TestCase
 {
--- tests/Json/UtfEncoderTest.php
+++ tests/Json/UtfEncoderTest.php
@@ -1,6 +1,5 @@
 <?php
 namespace Json;
-require_once(__DIR__ . '/../../bootstrap.php');
 
 class UtfEncoderTest extends \PHPUnit_Framework_TestCase
 {
