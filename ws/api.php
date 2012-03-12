d0324a696f75720582bd5e42cea2d9d63326a37d
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-12 11:06:02 +0100

    removed postlist, replaced with CsvStorage

--- ws/api.php
+++ ws/api.php
@@ -1,19 +1,14 @@
 <?php
+use Storage\CsvStorage;
 require_once(__DIR__ . '/../bootstrap.php');
 
 use Json\Codec;
 use Posts\PostList;
-use Validator\Profanity;
 
 header('Content-Type:application/json');
 
-$posts = Array();
-$list = new PostList(__DIR__ . '/../posts.csv');
-$validator = new Profanity();
-
-foreach ($list as $post) {
-    if ($validator->isValid($post->getText())) $posts[] = $post;
-}
+$storage = new CsvStorage('Posts\Post', __DIR__ . '/../posts.csv', Array('text'));
+$posts = $storage->findAll();
 
 $json = new Codec();
 echo $json->encode($posts);
