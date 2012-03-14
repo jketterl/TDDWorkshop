6e8fc096dca43eb01bd77d42e89e64846a0825e9
Jakob Ketterl <jketterl@chipxonio.de>
2012-03-14 21:15:25 +0100

    add tests & implementation

--- library/Validator/ValidatorCollection.php
+++ library/Validator/ValidatorCollection.php
@@ -3,8 +3,18 @@ namespace Validator;
 
 class ValidatorCollection implements ValidatorInterface
 {
+    private $_validators = Array();
+    
     public function isValid($data)
     {
-        
+        foreach ($this->_validators as $validator) {
+            if (!$validator->isValid($data)) return false;
+        }
+        return true;
+    }
+    
+    public function addValidator(ValidatorInterface $validator)
+    {
+        $this->_validators[] = $validator;
     }
 }
--- tests/Validator/ValidatorCollectionTest.php
+++ tests/Validator/ValidatorCollectionTest.php
@@ -10,8 +10,45 @@ class ValidatorCollectionTest extends \PHPUnit_Framework_TestCase
         $this->_collection = new ValidatorCollection();
     }
     
+    protected function getMockedValidator($return)
+    {
+        $mock = $this->getMock('Validator\ValidatorInterface');
+        $mock->expects($this->any())->method('isValid')->will($this->returnValue($return));
+        return $mock;
+    }
+    
     public function testInterfaceIsImplemented()
     {
         self::assertInstanceOf('Validator\ValidatorInterface', $this->_collection);
+    }
+    
+    public function testAddWorks()
+    {
+        $this->_collection->addValidator($this->getMockedValidator(true));
+    }
+    
+    public function testReturnsTrueWhenAllValidatorsPass()
+    {
+        // don't use c&p ;)
+        for ($i = 0; $i < 3; $i++)
+            $this->_collection->addValidator($this->getMockedValidator(true));
+        self::assertTrue($this->_collection->isValid('ambiguous'));
+    }
+    
+    public function testPassesDataToValidators()
+    {
+        $data = 'some arbitrary string';
+        $mock = $this->getMock('Validator\ValidatorInterface');
+        $mock->expects($this->any())->method('isValid')->with($data);
+        
+        $this->_collection->addValidator($mock);
+        $this->_collection->isValid($data);
+    }
+    
+    public function testReturnsFalseWhenOneValidatorFails()
+    {
+        $this->_collection->addValidator($this->getMockedValidator(true));
+        $this->_collection->addValidator($this->getMockedValidator(false));
+        self::assertFalse($this->_collection->isValid('ambiguous'));
     }
 }
