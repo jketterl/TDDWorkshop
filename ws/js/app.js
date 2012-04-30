966bef13cac3fc6cddaced668f1d9809a2c5dca4
Dominik Jungowski <Dominik@Jungowski.de>
2012-04-30 16:37:45 +0200

    Beitrag senden

--- ws/js/app.js
+++ ws/js/app.js
@@ -12,4 +12,13 @@ $(document).ready(function(){
         setTimeout(loadPosts, 10000);
     };
     loadPosts();
+
+	$('form').bind('submit', function(event) {
+		event.preventDefault();
+		var target = $(event.target);
+		$.ajax(target.attr('action'), {
+			type: 'post', 
+			data: target.serialize()
+		});
+	});
 });
